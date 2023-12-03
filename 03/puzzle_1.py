59932f0fd581064b8a2f4f59b1bd84e5dc3942c8
Mărășoiu Cosmin <marasoiu.cos@gmail.com>
2023-12-03 11:04:04 +0200

    day 3

---
+++ 03/puzzle_1.py
@@ -0,0 +1,26 @@
+sum = 0
+n = ""
+ok = False
+
+posx = (-1, -1, -1, 0, 1, 1, 1, 0)
+posy = (-1, 0, 1, 1, 1, 0, -1, -1)
+def isSymbol(x):
+    return x!='.' and not ( x in [str(i) for i in range(10)] )
+
+
+with open("input3.txt") as file:
+    M = file.read().splitlines()
+    for i,sublist in enumerate(M):
+        for j,val in enumerate(sublist):
+            if M[i][j].isdigit():
+                n+=M[i][j]
+                if ok is False:
+                    for k in range(8):
+                        try:
+                            if isSymbol(M[i + posx[k]][j + posy[k]]): ok = True
+                        except: continue
+            else:
+                if ok is True: sum+=int(n)
+                n = ""
+                ok = False
+print(sum)
