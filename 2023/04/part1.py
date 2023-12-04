014942b464a184d2e3ea0bd7bcdbae0f2fff5dfd
Mărășoiu Cosmin <marasoiu.cos@gmail.com>
2023-12-04 10:04:11 +0200

    day 4

---
+++ 2023/04/part1.py
@@ -0,0 +1,15 @@
+s = 0
+
+with open("input.txt") as file:
+    for card in file:
+        pts = 0
+        numbers = card.split(": ")[1].strip().split(" | ")
+        for win in numbers[0].split():
+            for mine in numbers[1].split():
+                if win==mine:
+                    if pts == 0:
+                        pts = 1
+                    else:
+                        pts *= 2
+        s+=pts
+print(s)
---
+++ 2023/04/part2.py
@@ -0,0 +1,18 @@
+s = 0
+amount = {}
+
+with open("input.txt") as file:
+    for card in file:
+        sp = card.split(": ")
+        cardId = int(sp[0].split()[1])
+        numbers = sp[1].strip().split(" | ")
+        count = cardId
+        amount[count] = amount.get(count, 1)
+        for win in numbers[0].split():
+            for mine in numbers[1].split():
+                if win==mine:
+                    count += 1
+                    amount[count] = amount.get(count, 1)
+                    amount[count] += amount[cardId]
+        s+=amount[cardId]
+print(s)
